14587ed85bd441e0398308b78fe028f87f247514
Heitor <m33n1213@gmail.com>
2026-02-19 13:08:00 -0300

    Protótipo do game_manager (não funciona ainda). Teste da feature de começar em um blank no main.py e organização dos imports e arquitetura

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import library
+from time import sleep
 
-library.testes()
+from rich.console import Console
+
+from models.table_renderer import Table_renderer
+from models.field_manager import Field_manager
+from models.game_manager import Game_manager
+
+console = Console()
+
+with console.status('Carregando...', spinner='aesthetic'):
+    fm = Field_manager()
+    tr = Table_renderer()
+    gm = Game_manager(console, fm, tr)
+    
+console.clear()
+console.rule('teste')
+fm.generate_basic_data(difficulty='difícil')
+fm.first_guess(5, 5)
+console.print(tr.render_visible_field(fm.render_data))
